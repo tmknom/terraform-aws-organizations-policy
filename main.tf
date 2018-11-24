db6acff32c05394d40768d7147c4aadabdd62520
tmknom <tmk.nom@gmail.com>
2018-11-24 23:22:46 +0900

    Implement Organizations Policy

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+# Terraform module which creates AWS Organizations Policy resources on AWS.
+#
+# https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies.html
 
+# https://www.terraform.io/docs/providers/aws/r/organizations_policy.html
+resource "aws_organizations_policy" "default" {
+  name        = "${var.name}"
+  description = "${var.description}"
+  type        = "${var.type}"
+  content     = "${data.aws_iam_policy_document.default.json}"
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    effect    = "Deny"
+    resources = ["*"]
+    actions   = ["${var.deny_actions}"]
+  }
+}
