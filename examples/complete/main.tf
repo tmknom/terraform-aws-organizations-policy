a3b5b33ce543b12752029b283c875cae237ff25d
tmknom <tmk.nom@gmail.com>
2018-11-25 00:53:52 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,1 +1,23 @@
+module "organizations_policy" {
+  source    = "../../"
+  name      = "example"
+  target_id = "${local.target_id}"
 
+  deny_actions = [
+    "cloudtrail:StopLogging",
+  ]
+
+  description = "Example policy"
+  type        = "SERVICE_CONTROL_POLICY"
+}
+
+locals {
+  target_id         = "${var.target_id != "" ? var.target_id : local.default_target_id}"
+  default_target_id = "ou-12345678"
+}
+
+variable "target_id" {
+  default     = ""
+  type        = "string"
+  description = "If TF_VAR_target_id set in the environment variables, then use that value."
+}
