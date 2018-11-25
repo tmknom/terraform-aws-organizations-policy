4f952705a304938da773302b0bd8c01f098d037b
tmknom <tmk.nom@gmail.com>
2018-11-25 09:51:38 +0900

    Use join instead of concat and element

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "organizations_policy_id" {
-  value       = "${element(concat(aws_organizations_policy.default.*.id, list("")), 0)}"
+  value       = "${join("", aws_organizations_policy.default.*.id)}"
   description = "The unique identifier (ID) of the policy."
 }
 
 output "organizations_policy_arn" {
-  value       = "${element(concat(aws_organizations_policy.default.*.arn, list("")), 0)}"
+  value       = "${join("", aws_organizations_policy.default.*.arn)}"
   description = "Amazon Resource Name (ARN) of the policy."
 }
